bb4c4fed274ded4c151df1c2e615518a5c96840d
DM-dev37 <dilanebertrand1@gmail.com>
2026-03-09 03:08:43 +0100

    composant innacheve il faudra remplacer l'ampoule

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [selectedColor, setSelectedColor] = useState("text-black");
+
   const colors = [
     {
       id: 1,
       label: "bleu",
-      fond: "text-blue-500 bg-blue-500 size-6 rounded-full",
+      fond: " bg-blue-500 size-6 rounded-full",
       colorClass: "text-blue-500",
     },
     {
       id: 2,
       label: "green",
-      fond: "text-green-500 bg-green-500 size-6 rounded-full",
+      fond: " bg-green-500 size-6 rounded-full",
       colorClass: "text-green-500",
     },
     {
       id: 3,
       label: "yellow",
-      fond: "text-yellow-500 bg-yellow-500 size-6 rounded-full",
-      colorClass: "text-yellow-500",
+      fond: " bg-yellow-500 size-6 rounded-full",
+      colorClass: "text-yellow-700",
     },
     {
       id: 4,
       label: "violet",
-      fond: "text-violet-600 bg-violet-500 size-6 rounded-full",
-      colorClass: "text-violet-500",
+      fond: " bg-violet-500 size-6 rounded-full",
+      colorClass: "text-violet-700",
     },
     {
       id: 5,
       label: "rose",
-      fond: "text-rose-900 bg-rose-500 size-6 rounded-full",
-      colorClass: "text-rose-500",
+      fond: " bg-rose-500 size-6 rounded-full",
+      colorClass: "text-rose-700",
     },
     {
       id: 6,
@@ -39,21 +41,36 @@ function App() {
       colorClass: "text-black",
     },
   ];
+
+  const handlecolorchange = (colorClass) => {
+    setSelectedColor(colorClass);
+  };
   return (
     <div className="h-screen flex items-center justify-center">
       <div className=" ">
         <h1 className="text-2xl font-black rounded-tl-xl rounded-br-xl text-black/70 p-6 bg-gradient-to-r from-green-400 via-blue-700 to-red-500">
           Changeons un peu de couleur
         </h1>
+
         <div className="mt-10 flex flex-col items-center justify-center">
-          <img
-            src="/public/lamp-fill.svg"
+          <div className={`mb-4 text-xl font-bold ${selectedColor}`}>
+            Test: {selectedColor}
+          </div>
+          {/* <img
+            src="/lamp-fill.svg"
             alt="lampe"
-            className="size-20 appearance-none forced-color-adjust-auto"
-          />
+            className={`size-20 ${selectedColor}`}
+          /> */}
           <div className="mt-10 space-x-8">
             {colors.map((color, index) => {
-              return <button key={index} className={color.fond} />;
+              return (
+                <button
+                  key={index}
+                  className={color.fond}
+                  onClick={() => handlecolorchange(color.colorClass)}
+                  aria-label={color.label}
+                />
+              );
             })}
           </div>
         </div>
